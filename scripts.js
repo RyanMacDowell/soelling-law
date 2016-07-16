8431d114bc057a4d183dd52e04ba70f250c1c17a
Ryan MacDowell <ryanmacdowell@gmail.com>
2016-07-15 23:13:18 -0400

    Moved script after css, improved responsive nav, TODO fix hardcoded sizes in css

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   var href = window.location.href;
   var page = href.substring(href.lastIndexOf("/")+1);
+  if (!page) {
+  	page = "index.htm";
+  }
   $(".main-nav [href='"+page+"']").addClass("current");
 });
 
